9fb9358569c2e3c3161dbfa2d23b3966e6a04ee4
lostone <lostone>
2015-05-28 05:37:48 +0000

    [02f9b47376] fix inserts

--- tcl/SQLInsert.tcl
+++ tcl/SQLInsert.tcl
@@ -23,12 +23,10 @@ nx::Class create SQLInsert -mixin [list SQLCommands]  {
 		}
 
 		set :sql "INSERT INTO $table ${:columns} VALUES ${:insert} "
-		puts "SQL is ${:sql}"
 
 		#If 1 or multiple primary keys, return.. otherwise don't return anything..
 		if {[dict exists ${:attributes} primarykey]} {
-			:insertPrimaryKeyReturning
-
+			set return [:insertPrimaryKeyReturning]
 		} else {
 			#Inserting values with no primary key..
 			set return  [dbi_dml -db ${:db} -bind ${:pr_stmt} ${:sql}] ;
@@ -46,11 +44,14 @@ nx::Class create SQLInsert -mixin [list SQLCommands]  {
 				if {[dict exists ${:attributes} sqlcolumns $key save]} {
 					if {![dict get ${:attributes} sqlcolumns $key save]}	 {  continue }
 				}
+
+				if {[:get $key] == ""} { continue  }
+
 				set separator [:getCondition ", "]
 				append columns [format "%s%s" $separator   $key  ]
 
 				set statement [:genStatement -type numeric $key [:get $key]]
-				append insert [format "%s%s" $separator $statement]
+				append insert [format "%s:%s" $separator $statement]
 			} 
 		}
 		set :columns "($columns)"
@@ -70,12 +71,13 @@ nx::Class create SQLInsert -mixin [list SQLCommands]  {
 	}
 
 	:method insertPrimaryKeyReturning {} {
-			append sql [:insertPrimaryKeyReturningPostgreSQL] 
+			append :sql [:insertPrimaryKeyReturningPostgreSQL] 
 			set returnid [dbi_0or1row -db ${:db} -array mydata -bind ${:pr_stmt} ${:sql}] 
 			if {$returnid == 0} { return false }
 			foreach id	[dict get ${:attributes} primarykey] {
 				my set $id $mydata($id)
 			}
+			return true
 	}
 
 	#Move these to a different mixin class..?
